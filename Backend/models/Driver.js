1b757ead276f5d19bb63a676c9795546315121f4
Vivek231213 <vivekvivjnv1@gmail.com>
2025-04-21 00:02:07 +0530

    backend and crud opration  connection eith DB

---
+++ Backend/models/Driver.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const driverSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [/\S+@\S+\.\S+/, 'Email is invalid']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters']
+  },
+  mobile: {
+    type: String,
+    required: [true, 'Mobile number is required'],
+    match: [/^\d{10}$/, 'Mobile number must be 10 digits']
+  },
+  licenseNo: {
+    type: String,
+    required: [true, 'Driving License Number is required']
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Driver', driverSchema);
